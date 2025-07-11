c8b9bb7572a0899b8508b0266d702b639df65670
MasterGhostDZ <87296911+Masterghostdz@users.noreply.github.com>
2025-07-11 17:02:01 +0100

    Update Android Version

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,31 +1,9 @@
-
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.7f88f98e6378401dbb0e993681c485d3',
-  appName: 'Logigrine',
-  webDir: 'dist',
-  server: {
-    url: 'https://7f88f98e-6378-401d-bb0e-993681c485d3.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      launchAutoHide: true,
-      backgroundColor: "#ffffffff",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: false,
-      androidSpinnerStyle: "large",
-      iosSpinnerStyle: "small",
-      spinnerColor: "#999999",
-      splashFullScreen: true,
-      splashImmersive: true,
-      layoutName: "launch_screen",
-      useDialog: true,
-    },
-  },
+  appId: 'com.logigrine.espace',
+  appName: 'logistic-app',
+  webDir: 'dist'
 };
 
 export default config;
